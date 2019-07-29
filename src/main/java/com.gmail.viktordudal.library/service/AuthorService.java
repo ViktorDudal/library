667068ff34196899090518bed0a75d2827fb4973
Viktor <viktordudal@gmail.com>
2019-07-29 14:41:38 +0300

    cleaned code

--- src/main/java/com.gmail.viktordudal.library/service/AuthorService.java
+++ src/main/java/com.gmail.viktordudal.library/service/AuthorService.java
@@ -2,18 +2,5 @@ package com.gmail.viktordudal.library.service;
 
 import com.gmail.viktordudal.library.model.Author;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface AuthorService extends BaseService<Author, Long>{
-
-//    Optional<Author> findById(Long id);
-//
-//    List<Author> findAll();
-//
-//    Author create(Author author);
-//
-//    Author update(Author author);
-//
-//    void deleteById(Long id);
 }
